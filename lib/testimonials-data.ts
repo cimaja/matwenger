e2417e57ecc0d5731a4800f52adbf29f12431529
Mathias <m.wendlinger@gmail.com>
2024-12-21 00:00:02 +0100

    Add new testimonials from John, Costas, Apostolis, and Matt
    - Add testimonials with profile photos
    - Update testimonial order

--- lib/testimonials-data.ts
+++ lib/testimonials-data.ts
@@ -2,6 +2,36 @@ import { Testimonial } from '@/types/testimonial';
 
 export const testimonials: Testimonial[] = [
   {
+    author: 'John Anastasopoulos',
+    role: 'GPM at Microsoft',
+    image: '/images/people/John_Anastasopoulos.png',
+    text: 'Mathias\'s dedication and leadership have been outstanding. His creative ideas and hard work, especially on the AI Copilot features, have significantly shaped the product. His ability to deliver on time and openness to feedback have kept projects on track and fostered continuous improvement. Mathias\'s technical depth and collaborative spirit have created a positive team environment and a smooth user experience. It\'s a privilege to work with him.'
+  },
+  {
+    author: 'Costas Chamosfakides',
+    role: 'Principal PM at Microsoft',
+    image: '/images/people/Costas_Chamosfakides.jpeg',
+    text: 'Working with Mathias over the past few months has been an absolute pleasure. He excels in taking ownership of features, delivering quality work and exceptional UI/UX experiences. Mathias is proactive, always thinking ahead and identifying potential issues early. His strong sense of the product\'s vision and strategy allows him to make valuable contributions. Additionally, he respects the diversity of the team, fostering a collaborative environment. I couldn\'t have asked for a better collaborator and look forward to more successful projects together!'
+  },
+  {
+    author: 'Apostolis Papaioannou',
+    role: 'Principal PM at Microsoft',
+    image: '/images/people/Apostolis_Papaioannou.jpeg',
+    text: 'Over the last year, collaborating with Mathias has been incredibly rewarding. His expertise in design and product experiences has greatly benefited our team. Mathias consistently delivers impressive outcomes, even under pressure, and his ability to ask the right questions enhances our product development.\n\nIn brainstorming sessions, he brings thought-provoking ideas, fostering exploration and innovation. His inclusive and growth mindset creates a collaborative team culture where all feedback is valued.\n\nI look forward to continuing our successful collaboration on future projects!'
+  },
+  {
+    author: 'Matt Shelton',
+    role: 'Principal Content Designer at Microsoft',
+    image: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e',
+    text: 'The team stands out for its ability to deliver important work that resonates with customers, despite geographical and time zone challenges. Mathias deserves full credit for this. He has a unique ability to hold people accountable without damaging their self-confidence or desire to improve. The team dynamic and the designers are fantastic, and being part of this team has been a joy. Full credit goes to Mathias for fostering such a positive and productive environment.'
+  },
+  {
+    author: 'Mohit Nair',
+    role: 'User Researcher at Microsoft',
+    image: '/images/people/Mohit_Nair.jpeg',
+    text: 'I\'ve really appreciated my partnership with Mathias so far. He embodies what it means to be a compassionate, thoughtful, and resourceful design leader. I admire the way he leads the design team and leverages research early in design planning. This proactive approach fosters a generative culture focused on product improvement. Mathias also takes time for his team, checks in on everyone, and embodies an empathetic mindset.'
+  },
+  {
     author: 'Antonin Djilali-Saiah',
     role: 'Senior Software Engineering at Microsoft',
     image: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e',
