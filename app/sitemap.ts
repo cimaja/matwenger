b36d40b9cf586d129ec5ec170d01026d3ded267c
Mathias <mwendlin+microsoft@microsoft.com>
2026-01-08 20:56:59 +0100

    Fix sitemap: add force-static export for static site generation

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next';
 import { getAllProjects } from '@/lib/get-project-content';
 
+export const dynamic = 'force-static';
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://matwenger.at';
   
